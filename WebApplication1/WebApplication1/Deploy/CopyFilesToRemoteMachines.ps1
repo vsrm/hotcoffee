e1a1809c164f75a7e9f50bd8ed9be0fa25d97ae3
vsrm@outlook.in <vsrm@outlook.in>
2015-07-18 15:40:43 +0530

    add separate script for dsc module install

---
+++ WebApplication1/WebApplication1/Deploy/CopyFilesToRemoteMachines.ps1
@@ -0,0 +1,10 @@
+﻿param
+(
+	# Location of the source deployment bits and DSC Module
+	[String]$SourcePath =  "C:\agent\_work\330fb393f\HotCoffee\WebApplication1\WebApplication1\bin",
+
+	# Destination path for the DSC module
+	[String]$dscModulePath = "C:\Program Files\WindowsPowerShell\Modules"
+)
+
+Copy-Item -Path "$SourcePath\DSCModule\xWebAdministration" -Destination "$dscModulePath" -Recurse -Force
